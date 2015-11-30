3c9be22b89bbbd7a9f5519c4ecec1a562c4c86ce
Mark Slone <markslone@outlook.com>
2015-11-29 19:10:31 -0500

    Create index.php
    the index page

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php 
+ require_once 'models/global.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mark Slone - Software Engineer</title>
+
+   <!--Import Google Icon Font-->
+     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <!--Import materialize.css-->
+     <link type="text/css" rel="stylesheet" href="css/materialize/css/materialize.min.css"  media="screen,projection"/>
+    
+
+     <!-- Start Meta -->
+     <meta charset="UTF-8">
+     <meta name="description" content="Learn how the web works, hire me to do it for you instead, or just browse what I've done.">
+     <meta name="keywords" content="HTML,CSS,XML,JavaScript,Web Developer,Educate,Learn,Develop,Portfolio,Software,Web Designer, Kendallville, IN">
+     <meta name="author" content="Mark Slone">
+     <!--Let browser know website is optimized for mobile-->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+     <!-- Favicons -->
+     <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=699rleQ96Q">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=699rleQ96Q" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png?v=699rleQ96Q" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png?v=699rleQ96Q" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=699rleQ96Q" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=699rleQ96Q" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=699rleQ96Q">
+<link rel="shortcut icon" href="/favicon.ico?v=699rleQ96Q">
+<meta name="msapplication-TileColor" content="#2d89ef">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png?v=699rleQ96Q">
+<meta name="theme-color" content="#ffffff">
+
+ <!-- MC styles -->
+ <link type="text/css" rel="stylesheet" href="css/mc.css" />
+ <!-- Animate.css -->
+ <link type="text/css" rel="stylesheet" href="css/animate.css" />
+
+</head>
+<body>
+
+  <!-- Header / Nav-bar -->
+  <?php include_once 'layout/nav.php'; ?>
+
+
+ 
+   
+   <div class="section img-bg animated fadeInDownBig"> 
+
+      <div class="info-con">
+      <h1 class="center white-text animated fadeInDown">Hello, My name is Mark.</h1>
+      <p class="center white-text animated slideInLife">I create software, and aim to build something that positively changes lives for the better.<br><br>
+      <a href="https://www.behance.net/markslonef089" class="ghost-button animated slideInLeft">View My Work</a>
+      <a href="about" class="ghost-button animated slideInRight">About Me</a>
+      </div>
+
+   </div> 
+
+   <!-- Portfolio -->
+
+   <div class="container">
+     <div class="section">
+
+     <div class="row">
+
+       <div class="col s12 l4">
+     <div class="card-panel light-green  small"> 
+       <p class="center white-text"><i class="large material-icons">build</i></p>
+       <h3 class="center white-text">Engineer.</h3>
+       <p class="center white-text">Not just any engineer. A software engineer.</p>
+     </div>
+   </div>
+
+
+
+     <div class="col s12 l4">
+   <div class="card-panel deep-purple  small">
+     <p class="center white-text"><i class="large material-icons">color_lens</i></p>
+     <h3 class="center white-text">Designer.</h3>
+     <p class="center white-text">Design is in my blood. In every line of code.</p>
+   </div>
+ </div>
+
+
+      <div class="col s12 l4">
+    <div class="card-panel orange  small">
+      <p class="center white-text"><i class="large material-icons">code</i></p>
+      <h3 class="center white-text">Coder.</h3>
+      <p class="center white-text">Dechiper every little bug. With love and passion, passionately.</p>
+    </div>
+  </div>
+
+
+ </div>
+
+    </div>
+ </div>
+
+
+  <!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="css/materialize/js/materialize.js"></script>
+<script src="js/global.js"></script>
+
+
+</body>
+</html>
