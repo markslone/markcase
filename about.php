edd24046c36786c8e43eb5ff476aa5408db8c634
Mark Slone <markslone@outlook.com>
2015-11-29 19:12:38 -0500

    create about.php
    the about page

---
+++ about.php
@@ -0,0 +1,145 @@
+<?php 
+ require_once 'models/global.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mark Slone - About</title>
+      <!-- Start Meta -->
+     <meta charset="UTF-8">
+     <meta name="description" content="Learn how the web works, hire me to do it for you instead, or just browse what I've done.">
+     <meta name="keywords" content="HTML,CSS,XML,JavaScript,Web Developer,Educate,Learn,Develop,Portfolio,Software,Web Designer">
+     <meta name="author" content="Mark Slone">
+     <!--Let browser know website is optimized for mobile-->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+     <!-- Favicons -->
+     <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=699rleQ96Q">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=699rleQ96Q">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=699rleQ96Q" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png?v=699rleQ96Q" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png?v=699rleQ96Q" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=699rleQ96Q" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=699rleQ96Q" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=699rleQ96Q">
+<link rel="shortcut icon" href="/favicon.ico?v=699rleQ96Q">
+<meta name="msapplication-TileColor" content="#2d89ef">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png?v=699rleQ96Q">
+<meta name="theme-color" content="#ffffff">
+
+  <!--Import Google Icon Font-->
+     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!--Import materialize.css-->
+     <link type="text/css" rel="stylesheet" href="css/materialize/css/materialize.min.css"  media="screen,projection"/>
+     <!-- Import MC -->
+     <link type="text/css" rel="stylesheet" href="css/mc.css"  media="screen,projection"/>
+     <!-- Import Animate.css -->
+     <link type="text/css" rel="stylesheet" href="css/animate.css"  media="screen,projection"/>
+     
+
+
+
+     
+</head>
+<body>
+
+  
+
+  <!-- Header / Nav-bar -->
+  <?php include_once 'layout/nav.php'; ?>
+
+  <!-- Page Header -->
+   <div class="section about-bg animated slideInDown">
+      <h1 class="center white-text">About me</h1>
+      <p class="center white-text animated slideInDown">An autobiography in the makes.<br><br>
+      <a class="btn light-blue animated slideInleft" href="https://twitter.com/kingdizzalo">Twitter</a>
+      <a class="btn blue darken-2 animated slideInRight" href="https://behance.net/markslonef089">Behance</a>
+     </p>
+   </div>
+  <!-- End Page Header -->
+
+  <!-- Bio -->
+  <div class="section"> 
+     <div class="container">
+
+    <div class="row">
+    <div class="col s12">
+      <h3>Who am I?</h3>
+      <p>In July of 1997 on the 30th day, a child named Mark A Slone was born. With days, weeks and years of coding and designing as a hobby. This child grew into a man,     <br>  a man who developed a love for the web, a connection, a need to be a part of the web. And so to this day, this man strives to find a place in the ever-changing world of selfies and egotistic he said she said. Because beyond all that is a stronger purpose and use, even need for technology and the humans behind it.  </p>
+    </div>
+   </div>
+   <!-- End Bio -->
+
+
+    <!-- Skill Set -->
+    <div class="row">
+    <div class="col m7 l9 s12">
+    <h3>Skill Set</h3>
+     <p>
+      <label>HTML5</label> <div class="progress">
+      <div class="determinate light-blue" style="width: 100%"></div>
+      </div>
+     </p>
+
+      <p>
+      <label>CSS</label> <div class="progress">
+      <div class="determinate light-green" style="width: 97%"></div></div>
+     </p>
+
+     <p>
+      <label>RUBY</label> <div class="progress">
+      <div class="determinate red lighten-1" style="width: 19%"></div></div>
+     </p>
+
+     <p>
+      <label>PHP</label> <div class="progress">
+      <div class="determinate deep-purple" style="width: 95%"></div></div>
+     </p>
+
+     <p>
+      <label>JAVASCRIPT</label> <div class="progress">
+      <div class="determinate amber" style="width: 79%"></div></div>
+     </p>
+
+     <p>
+      <label>PHOTOSHOP</label> <div class="progress">
+      <div class="determinate indigo" style="width: 89%"></div></div>
+     </p>
+
+
+    </div>
+    </div>
+   <!-- End Skill Set -->
+
+    <!-- Download Resume -->
+    <div class="row">
+    <div class="col m7 l9 s12">
+    <h3>Download Resume</h3>
+     <a href="Resume.docx" class="btn-large light-green" download> <i class="material-icons left"> file_download </i> Download Resume  </a>
+    </div>
+    </div>
+    <!-- End Download Resume -->
+    
+   <!-- End Section & Container -->
+   </div>
+  </div>
+
+  
+
+
+  <!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="css/materialize/js/materialize.js"></script>
+<script src="js/global.js"></script>
+
+
+</body>
+</html>
